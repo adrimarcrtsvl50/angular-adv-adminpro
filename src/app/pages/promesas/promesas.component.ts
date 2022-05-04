38bddfdb067fc157d5097d4cf290dfcaa20027e4
Adriana Marcela Cortes <53945884+adrimarcrtsvl50@users.noreply.github.com>
2022-05-04 16:05:05 -0500

    reforzamiento sobre promesa

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const promesa = new Promise( (resolve, reject  ) => {
+
+      if ( false ) {
+            resolve('Hola Mundo');
+           } else {
+             reject('Algo salio mal');
+         }
+      
+
+    });
+    console.log('Fin del Init');
+     promesa.then( (mensaje) => {
+        console.log( mensaje );
+       }).catch( error => console.log('Error en mi promesa', error ) );
+
+  }
+     
+    
+
+  
+}
