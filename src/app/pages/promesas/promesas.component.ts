bda8581201e9587559c9bf7df202da0f839e35cf
Adriana Marcela Cortes <53945884+adrimarcrtsvl50@users.noreply.github.com>
2022-05-10 10:17:59 -0500

    rxjs

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -11,7 +11,10 @@ export class PromesasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const promesa = new Promise( (resolve, reject  ) => {
+    this.getUsuarios().then( usuarios => {
+      console.log(usuarios);
+    })
+    /* const promesa = new Promise( (resolve, reject  ) => {
 
       if ( false ) {
             resolve('Hola Mundo');
@@ -25,9 +28,19 @@ export class PromesasComponent implements OnInit {
      promesa.then( (mensaje) => {
         console.log( mensaje );
        }).catch( error => console.log('Error en mi promesa', error ) );
+ */
+  }
+  getUsuarios() {
+
+    return new Promise( resolve => {
+
+      fetch('https://reqres.in/api/users')
+        .then( resp => resp.json() )
+        .then( body => resolve( body.data ) );
+
+    });
 
   }
-     
     
 
   
